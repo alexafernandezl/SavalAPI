6861edc03d435b0903c7d37b8305ab372d1f472c
alexafernandezl <aleferlo14@gmail.com>
2025-02-12 21:09:22 -0600

    Respuesta

--- Models/DetalleRespuesta.cs
+++ Models/DetalleRespuesta.cs
@@ -1,16 +1,24 @@
-﻿namespace SavalAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SavalAPI.Models
 {
     public class DetalleRespuesta
     {
         public int IdDetalleRespuesta { get; set; }
 
         public int IdRespuesta { get; set; }
-        public Respuesta Respuesta { get; set; }
+
+        [JsonIgnore]
+        public Respuesta? Respuesta { get; set; }
 
         public int IdPregunta { get; set; }
-        public Pregunta Pregunta { get; set; }
+
+        [JsonIgnore]
+        public Pregunta? Pregunta { get; set; }
 
         public int IdOpcion { get; set; }
-        public OpcionRespuesta Opcion { get; set; }
+
+        [JsonIgnore]
+        public OpcionRespuesta? Opcion { get; set; }
     }
 }
